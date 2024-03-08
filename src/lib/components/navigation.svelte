1d17e224da48b41134b5967e518c10d41580692a
mike-walpole <walpole.mike@gmail.com>
2024-03-08 20:40:59 +0100

    links

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -1,10 +1,10 @@
 <body class="bg-[#FFFDF5]"> 
 <div class="grid grid-cols-2 max-w-4xl justify-between items-center bg-[#FFFDF5] py-4 mx-auto z-50">
-	<a href="/"><img src="/gorman-horizontal.webp" class="mr-3 h-6 sm:h-6" alt="PCTU Logo" /></a>
+	<a href="https://gorman.pl"><img src="/gorman-horizontal.webp" class="mr-3 h-6 sm:h-6" alt="PCTU Logo" /></a>
 	
 	<div class="flex justify-end mr-1">
 		<a
-			href="https://www.gorman.pl/"
+			href="https://test.gorman.pl/rezerwacja"
 			class="border text-white bg-[#911B53] text-sm rounded-lg my-1 px-3 py-2 w-max-md grid content-center"
 			>Jak uzyskać pomoc ↗</a
 		>
