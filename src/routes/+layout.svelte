b3abf6d461cc26ccb19c2404fd52b09894f1cd37
mike-walpole <walpole.mike@gmail.com>
2024-03-13 00:49:33 +0100

    segment and rudderstack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,25 @@
   analytics.page();
   }}();
 
+ 
+        !function(){"use strict";var sdkBaseUrl="https://cdn.rudderlabs.com/v3";var sdkName="rsa.min.js";var asyncScript=true
+        ;window.rudderAnalyticsBuildType="legacy",window.rudderanalytics=[]
+        ;var e=["setDefaultInstanceKey","load","ready","page","track","identify","alias","group","reset","setAnonymousId","startSession","endSession"]
+        ;for(var t=0;t<e.length;t++){var n=e[t];window.rudderanalytics[n]=function(e){return function(){
+        window.rudderanalytics.push([e].concat(Array.prototype.slice.call(arguments)))}}(n)}try{
+        new Function('return import("")'),window.rudderAnalyticsBuildType="modern"}catch(a){}
+        if(window.rudderAnalyticsMount=function(){
+        "undefined"==typeof globalThis&&(Object.defineProperty(Object.prototype,"__globalThis_magic__",{get:function get(){
+        return this},configurable:true}),__globalThis_magic__.globalThis=__globalThis_magic__,
+        delete Object.prototype.__globalThis_magic__);var e=document.createElement("script")
+        ;e.src="".concat(sdkBaseUrl,"/").concat(window.rudderAnalyticsBuildType,"/").concat(sdkName),e.async=asyncScript,
+        document.head?document.head.appendChild(e):document.body.appendChild(e)
+        },"undefined"==typeof Promise||"undefined"==typeof globalThis){var d=document.createElement("script")
+        ;d.src="https://polyfill.io/v3/polyfill.min.js?version=3.111.0&features=Symbol%2CPromise&callback=rudderAnalyticsMount",
+        d.async=asyncScript,document.head?document.head.appendChild(d):document.body.appendChild(d)}else{
+        window.rudderAnalyticsMount()}window.rudderanalytics.load("2daonypjdAKMmY7iErlfQTx6Wqt","https://gormanmicurxlr.dataplane.rudderstack.com",{})}();    
+
+
 			
 		}, 5000);
 	</script>
